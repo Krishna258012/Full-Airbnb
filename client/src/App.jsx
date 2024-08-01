5be06c2ad5e40c2db51d421b629130bfd6589e9a
Krishna258012 <121844303+Krishna258012@users.noreply.github.com>
2024-08-01 15:38:49 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,30 @@
-
-import {  Routes, Route, useLocation } from 'react-router-dom'
-import './App.css'
-import Header from './Components/Header'
-import HOmepage from './Components/HOmepage'
-import Search from './Components/Search'
-import Login from './Components/Login'
+import { Routes, Route, useLocation } from 'react-router-dom';
+import './App.css';
+import Header from './Components/Header';
+import HOmepage from './Components/HOmepage';
+import Search from './Components/Search';
+import Login from './Components/Login';
 
 function App() {
   const location = useLocation();
   const noHeaderPaths = ['/account'];
+
+  console.log("Current Pathname:", location.pathname); // Debugging log
+
   const shouldShowHeader = !noHeaderPaths.includes(location.pathname.toLowerCase());
+
+  console.log("Should Show Header:", shouldShowHeader); // Debugging log
+
   return (
     <>
       {shouldShowHeader && <Header />}
-        <Routes>
-          <Route path="/" element={<HOmepage />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/account" element={<Login/>} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<HOmepage />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/account" element={<Login />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
