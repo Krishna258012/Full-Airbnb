98eaaa9730c6bfb51b40789ad02d92322b637981
unknown <krishna258012@gmail.com>
2024-08-01 15:27:15 +0530

    24

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,24 @@
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {  Routes, Route, useLocation } from 'react-router-dom'
 import './App.css'
 import Header from './Components/Header'
 import HOmepage from './Components/HOmepage'
 import Search from './Components/Search'
+import Login from './Components/Login'
 
 function App() {
+  const location = useLocation();
+  const noHeaderPaths = ['/account'];
 
-
+  const shouldShowHeader = !noHeaderPaths.includes(location.pathname.toLowerCase());
   return (
     <>
-      <Router>
-        <Header />
+      {shouldShowHeader && <Header />}
         <Routes>
           <Route path="/" element={<HOmepage />} />
-          <Route path={"/search"} element={<Search />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/Account" element={<Login/>} />
         </Routes>
-      </Router>
-
-      {/* <Home/> */}
     </>
   )
 }
